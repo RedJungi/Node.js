6289edb3dcea071454b595fbc4dee83586cfa77c
홍준기 <101107394+RedJungi@users.noreply.github.com>
2025-08-05 20:53:12 +0900

    8.5 추가 사항

--- src/socketServer.js
+++ src/socketServer.js
@@ -1,18 +1,56 @@
-const express = require("express");
-const WebSocket = require("ws"); // WebSocket 모듈을 가져온다
-const app = express();
+const express = require("express"); // express 모듈
+const http = require("http");
+const { Client } = require("ssh2"); // ssh모듈
+const { Server } = require("socket.io");
+const path = require("path"); // 경로 모듈
 
-app.use("/", (req, res) => {
-  res.sendFile(__dirname + "/socket.html");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const linux = {
+  host: "192.168.105.131",
+  port: 22,
+  username: "hjg",
+  password: "1234",
+};
+
+app.use(express.static("public")); // 정적 파일 제공
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "socket.html")); // 클라이언트 HTML 파일 제공
 });
 
-app.listen(8080);
+io.on("connection", (socket) => {
+  console.log("클라이언트 접속");
 
-const socket = new WebSocket.Server({ port: 8081 }); // WebSocket 서버를 8081 포트에서 실행
+  const conn = new Client(); // ssh2 모듈의 Client 클래스를 사용하여 SSH 클라이언트 인스턴스를 생성
 
-socket.on("connection", (ws, req) => {
-  ws.on("message", (msg) => {
-    console.log("받은 메세지:" + msg);
-    ws.send("서버에서 응답: " + msg);
+  conn.on("ready", () => {
+    console.log("SSH 연결 완료");
+
+    conn.exec("ps", (err, stream) => {
+      if (err) {
+        socket.emit("error", { message: "명령어 실행 오류" });
+        return;
+      }
+      stream.on("data", (data) => {
+        socket.emit("pslog", data.toString());
+      });
+
+      stream.stderr.on("data", (data) => {
+        socket.emit("error", data.toString());
+      });
+
+      socket.on("disconnect", () => {
+        console.log("클라이언트 연결 종료");
+        stream.close();
+        conn.close();
+      });
+    });
   });
+  conn.connect(linux); // SSH 서버에 연결
+});
+server.listen(3000, () => {
+  console.log("서버가 3000 포트에서 실행");
 });
